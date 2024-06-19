f575074a56639152eef32ae1e25626c7298951a0
Ito Junpei <123927296+itojun-1230@users.noreply.github.com>
2024-06-19 14:02:36 +0900

    update: port3000でHTMLにルーティング

--- server.ts
+++ server.ts
@@ -1,1 +1,17 @@
-console.log("Hello TypeScript!");
+//ライブラリ
+import express from 'express';
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const port = 3000;
+app.listen(port, () => {
+
+  console.log(`Start the server at Port${port}`);
+});
+
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/${process.env.PUBLIC_HTMLFILE}`);
+});
