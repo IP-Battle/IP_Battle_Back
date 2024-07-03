806a8329ee23bb124a721fea5e3ea941939dc220
Ito Junpei <123927296+itojun-1230@users.noreply.github.com>
2024-07-03 13:27:32 +0900

    add: questionType

---
+++ src/types/question.ts
@@ -0,0 +1,11 @@
+export type questionType = {
+  id: number,
+  questionText: string,
+  aSelect: string,
+  iSelect: string,
+  uSelect: string,
+  eSelect: string,
+  answer: string,
+  explanation: string,
+  accuracyRate: number
+};
